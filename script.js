219b2dc68d1ad583833fd79443b793d82999e727
drparnassus <drparnassus@gmail.com>
2024-04-19 18:00:11 -0500

    add basic limited functions

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // calculator script for index.html
+
+function add(x,y) {
+    return x+y;
+}
+
+function subtract(x,y) {
+    return x-y;
+}
+
+function multiply(x,y) {
+    return x*y;
+}
+
+function divide(x,y) {
+    if (y==0) {
+        return "ERROR"
+    }
+    else {
+        return x/y;
+    }
+}
+
+console.log(add(1,7));
+console.log(subtract(1,7));
+console.log(multiply(1,7));
+console.log(divide(1,7));
+console.log(add(0.1,0.2));
