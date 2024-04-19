065230c2343c1226525116a3faa96beaa679781f
drparnassus <drparnassus@gmail.com>
2024-04-19 18:18:35 -0500

    add operate functions and required variables

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // calculator script for index.html
 
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
 function add(x,y) {
     return x+y;
-}
+};
 
 function subtract(x,y) {
     return x-y;
-}
+};
 
 function multiply(x,y) {
     return x*y;
-}
+};
 
 function divide(x,y) {
     if (y==0) {
@@ -19,10 +23,35 @@ function divide(x,y) {
     else {
         return x/y;
     }
-}
+};
 
-console.log(add(1,7));
+/*console.log(add(1,7));
 console.log(subtract(1,7));
 console.log(multiply(1,7));
 console.log(divide(1,7));
-console.log(add(0.1,0.2));
+console.log(add(0.1,0.2));*/
+
+function operate(a, op, b) {
+    switch (op) {
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return subtract(a,b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+        default:
+            break;
+    }
+};
+
+firstNum = 2;
+secondNum = 5;
+operator = "*";
+
+console.log(operate(firstNum, operator, secondNum));
